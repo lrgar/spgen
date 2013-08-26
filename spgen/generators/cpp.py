3550d81367bbc18680057fbd48ea35baad07c305
Luis Garcia <lgarcia14@gmail.com>
2013-08-26 14:52:19 -0430

    Start code generation

---
+++ spgen/generators/cpp.py
@@ -0,0 +1,8 @@
+#
+# spgen.py
+#
+# Copyright (c) 2013 Luis Garcia.
+# This source file is subject to terms of the MIT License. (See accompanying file LICENSE)
+#
+
+"""C++ compiler code generator."""
