e08fb64b591cf829f4bb59b41e562d97a0bd5bf7
Luis Garcia <lgarcia14@gmail.com>
2013-08-26 14:50:17 -0430

    Start support for command

--- spgen/spgen.py
+++ spgen/spgen.py
@@ -7,13 +7,63 @@
 
 """Compiler generator script for C++."""
 
-from spgen_parser import *
+import spgen_parser as parser
+import spgen_processor as processor
+import generators
+import getopt
+import sys
+
+version = '0.1.0-alpha'
+
+class CommandArgumentsError(Exception):
+	pass
+
+def read_arguments(args):
+	grammar_file = ''
+	output_language = ''
+
+	try:
+		opts, args = getopt.getopt(args, 'g:l:', [ 'grammar=lang=' ])
+		for opt, arg in opts:
+			if opt in ('-g', '--grammar'):
+				grammar_file = arg
+			elif opt in ('-l', '--lang'):
+				output_language = arg
+	except getopt.GetoptError as err:
+		raise CommandArgumentsError(err.msg)
+
+	if grammar_file == '' or output_language == '':
+		raise CommandArgumentsError('You must specify an input grammar file and the output language.')
+
+	return grammar_file, output_language
+
+def usage():
+	print('Usage: python spgen.py -g <input-file>')
+	print('Version: ' + version)
+	print('Copyright (c) 2013 Luis Garcia')
+	print('')
+	print('See README for more info.')
 
 def main(args):
-	parser = Parser()
-	context = Context()
-	#context = parser.process_file('')
-	pass
+	try:
+		grammar_file, output_language = read_arguments(args)
+
+		context = parser.Context()
+		context.properties['outputLanguage'] = output_language
+		parser.Parser().process_file(grammar_file, context)
+		nfa_graph = processor.NFAGraphGenerator().generate(context)
+		dfa_graph = processor.DFAGraphGenerator().generate(nfa_graph)
+		transition_table = processor.TransitionTableGenerator().generate(dfa_graph)
+
+	except parser.ParserError as err:
+		print('Parse error: {0}'.format(err.args[0]))
+		sys.exit(2)
+
+	except CommandArgumentsError as err:
+		print('Error: {0}'.format(err.args[0]))
+		print('')
+		usage()
+		sys.exit(2)
 
 if __name__ == '__main__':
 	main(sys.argv[1:])
