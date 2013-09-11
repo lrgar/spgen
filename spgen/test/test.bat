5cdfd6d7a4571ccef6212c6c956dede409b8b441
Luis Garcia <lgarcia14@gmail.com>
2013-09-11 17:49:34 -0430

    Include generated source for compilation test

--- spgen/test/test.bat
+++ spgen/test/test.bat
@@ -1,3 +1,3 @@
 python ..\spgen.py -l cpp -g .\test01.spg
-cl /Fo.\gen\ /EHsc Test01.cpp /link /out:.\gen\test01.exe
+cl /Fo.\gen\ /EHsc Test01.cpp gen/Test01Parser.cpp /link /out:.\gen\test01.exe
 gen\test01
